b6e4faf94f4b7fc9ae6b8483bd768aa65a78db0b
Sapphire Smith <ssapphire527@gmail.com>
2023-05-26 13:54:21 +0530

    logo import error

--- client/src/components/LoginSignupNav.jsx
+++ client/src/components/LoginSignupNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../assets/Logo.svg'
+import Logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
 const LoginSignupNav = () => {
   return (
