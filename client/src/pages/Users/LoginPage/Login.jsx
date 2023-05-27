9df1a2f831a1953a158e7710c847a3a7b93b8bcf
Sapphire Smith <ssapphire527@gmail.com>
2023-05-27 11:08:05 +0530

    ..

--- client/src/pages/Users/LoginPage/Login.jsx
+++ client/src/pages/Users/LoginPage/Login.jsx
@@ -56,27 +56,27 @@ const Login = () => {
       />
 
       <div className='bg-[#072541]'>
-        <div className='max-w-[1400px] mx-auto h-[100vh] flex justify-center items-center '>
-          <div className='mx-auto '>
+        <div className='max-w-[1400px] mx-auto h-screen flex justify-center items-center '>
+          <div className='mx-auto'>
             <h1 className='text-white text-center font-medium text-lg'>Welcome Back <span className='ml-1'> :)</span></h1>
-            <form className='flex flex-col mx-8 gap-5 pt-6 sm:w-[300px] lg:w-[400px]'
+            <form className='flex flex-col w-full justify-center gap-5 pt-6  sm:w-[300px] lg:w-[400px]'
               onSubmit={formik.handleSubmit}>
-              <div className='flex flex-col gap-2'>
+              <div className='flex flex-col gap-2 w-full'>
                 <label className='text-white font-thin text-lg'>Email</label>
                 <input type="email" required placeholder='email' className='px-3 py-1 rounded-md font-normal focus:outline-none'
                   {...formik.getFieldProps('email')} />
               </div>
-              <div className='flex flex-col gap-2'>
+              <div className='flex flex-col gap-2 w-full'>
                 <label className='text-white font-thin text-lg'>Password</label>
                 <input type="password" required placeholder='password' className='px-3 py-1 rounded-md font-normal focus:outline-none'
                   {...formik.getFieldProps('password')} />
               </div>
-              <div className='flex pt-4'>
-                <button type='submit' className='text-white mx-auto bg-blue-400 w-full py-1 rounded-md font-semibold duration-300 hover:bg-[#015bbb]'>Login</button>
+              <div className='flex pt-4 w-full'>
+                <button type='submit' className='text-white mx-auto w-full bg-blue-400  py-1 rounded-md font-semibold duration-300 hover:bg-[#015bbb]'>Login</button>
               </div>
             </form>
             <div className='text-center pt-4'>
-              <p className='text-white font-semibold'>Not a Member yet? <Link className='font-semibold text-[#4ea3ff] hover:text-[#015bbb] duration-150' to={'/user/signup'}>Register</Link></p>
+              <p className='text-white font-semibold text-xs'>Not a Member yet? <Link className='font-semibold text-[#4ea3ff] hover:text-[#015bbb] duration-150' to={'/user/signup'}>Register</Link></p>
             </div>
           </div>
         </div>
