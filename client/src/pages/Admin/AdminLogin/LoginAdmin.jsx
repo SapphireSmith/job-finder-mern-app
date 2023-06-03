8574d6ad22eaf83cb7e5e1454052a1bf433ec334
Sapphire Smith <ssapphire527@gmail.com>
2023-06-03 09:54:35 +0530

    login bug

--- client/src/pages/Admin/AdminLogin/LoginAdmin.jsx
+++ client/src/pages/Admin/AdminLogin/LoginAdmin.jsx
@@ -17,6 +17,7 @@ const LoginAdmin = () => {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
+      console.log(values);
       let loginPromise = adminLogin(values);
 
       toast.promise(loginPromise, {
@@ -50,11 +51,11 @@ const LoginAdmin = () => {
         }}
       />
 
-      <div className='bg-[#072541]'>
+      {/* <div className='bg-[#072541]'>
         <div className='max-w-[1400px] mx-auto h-[100vh] flex justify-center items-center '>
-          <div className='mx-auto '>
             <h1 className='text-white text-center font-medium text-lg md:text-2xl'>Admin Login</h1>
-            <form className='flex flex-col mx-8 gap-5 pt-6 w-[1/2] sm:w-[300px] lg:w-[400px]'
+          <div className='mx-auto  '>
+            <form className='flex flex-col gap-5 pt-6 px-5 w-full sm:w-1/2 lg:w-1/2'
               onSubmit={formik.handleSubmit}>
               <div className='flex flex-col gap-2'>
                 <label className='text-white font-thin text-lg'>Email</label>
@@ -71,6 +72,28 @@ const LoginAdmin = () => {
               </div>
             </form>
           </div>
+        </div>
+      </div> */}
+
+      <div className='bg-[#072541]'>
+        <div className='max-w-[1120px] h-screen flex flex-col mx-auto  justify-center items-center '>
+          <h1 className='text-white text-center font-medium text-lg'>Admin Login</h1>
+          <form className='flex flex-col gap-5 pt-6 px-5 w-full sm:w-1/2 lg:w-1/2'
+            onSubmit={formik.handleSubmit}>
+            <div className='flex flex-col gap-2 w-full'>
+              <label className='text-white font-thin text-lg'>Email</label>
+              <input type="email" required placeholder='email' className='px-3 py-1 rounded-md font-normal focus:outline-none'
+                {...formik.getFieldProps('email')} />
+            </div>
+            <div className='flex flex-col gap-2 w-full'>
+              <label className='text-white font-thin text-lg'>Password</label>
+              <input type="password" required placeholder='password' className='px-3 py-1 rounded-md font-normal focus:outline-none'
+                {...formik.getFieldProps('password')} />
+            </div>
+            <div className='flex pt-4 w-full'>
+              <button type='submit' className='text-white mx-auto w-full bg-blue-400  py-1 rounded-md font-semibold duration-300 hover:bg-[#015bbb]'>Login</button>
+            </div>
+          </form>
         </div>
       </div>
 
--- client/src/pages/Users/LoginPage/Login.jsx
+++ client/src/pages/Users/LoginPage/Login.jsx
@@ -58,7 +58,7 @@ const Login = () => {
       <div className='bg-[#072541]'>
         <div className='max-w-[1120px] h-screen flex flex-col mx-auto  justify-center items-center '>
             <h1 className='text-white text-center font-medium text-lg'>Welcome Back <span className='ml-1'> :)</span></h1>
-            <form className='flex flex-col mx-8 gap-5 pt-6 w-[1/2] sm:w-[300px] lg:w-[400px]'
+            <form className='flex flex-col gap-5 pt-6 px-5 w-full sm:w-1/2 lg:w-1/2'
               onSubmit={formik.handleSubmit}>
               <div className='flex flex-col gap-2 w-full'>
                 <label className='text-white font-thin text-lg'>Email</label>
